dd517d798678b68b9a040cccf5d1b9e5fa04570f
Temus <temus@wir.pl>
2020-03-28 14:44:17 +0100

    Add test for registerUser method

---
+++ src/test/java/DelegationService/Service/RegisterUserTest.java
@@ -0,0 +1,46 @@
+package DelegationService.Service;
+
+import DelegationService.Model.User;
+import DelegationService.Repository.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class RegisterUserTest {
+
+    @Autowired
+    private UserRepository testUserRepository;
+
+    @Autowired
+    private UserService testUserService;
+
+    @Test
+    public void registerUser() {
+        User testUser = new User(
+                "Grupa 4",
+                "Kaliskiego 6/9",
+                "123456789",
+                "Maurycy",
+                "Łamignat",
+                "123@wp.pl",
+                "admin1234");
+
+        testUserService.registerUser(testUser);
+
+        List<User> foundUsers = testUserRepository.findAll();
+
+        for (User u: foundUsers) {
+            if (u.equals(testUser)) {
+                Assertions.assertThat(u).isEqualTo(testUser);
+            }
+        }
+
+    }
+}
